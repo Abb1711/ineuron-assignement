28d713978abeb33b3c188519bd5bf3a1cf73dc7a
Abb1711 <93313196+Abb1711@users.noreply.github.com>
2022-08-10 16:46:02 +0530

    Add files via upload

---
+++ assingment 01/A1(Q4).C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ float R;
+  float A;
+   scanf("%f",&R);
+   A=3.14*R*R;
+   printf("\"Area of circle is %f having the radius %f\"",A,R);
+
+}
---
+++ assingment 01/A1(Q5).C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int x =printf("String\n");
+    printf("%d",x);
+    return 0;
+}
+
